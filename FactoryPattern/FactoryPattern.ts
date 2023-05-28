44ab0cddce5817d44903ee56a16c5737eb6b5b1e
Leon <Leon.G@binarywalk.com>
2023-05-28 10:58:07 +0800

    FactoryPatern comit

---
+++ FactoryPattern/FactoryPattern.ts
@@ -0,0 +1,16 @@
+let countNum1 = 123;
+
+let countNum2 = 22;
+
+let calSymbol = '+';
+
+switch (calSymbol) {
+    case '+':
+        break;
+    case '-':
+        break;
+    case '-':
+        break;
+    case '-':
+        break;
+}
